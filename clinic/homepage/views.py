380f7007acb9f23e588c312191c8841922e72f22
sadegh <sadegh.moayedizadeh@gmail.com>
2024-02-06 22:23:57 +0330

    Parametrize templates.

--- clinic/homepage/views.py
+++ clinic/homepage/views.py
@@ -1,4 +1,17 @@
+import os
+
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, "index.html")
+    content_directory = 'clinic/homepage/content'
+    content_data = {}
+
+    for filename in os.listdir(content_directory):
+        if filename.endswith('.txt'):
+            file_path = os.path.join(content_directory, filename)
+            with open(file_path, 'r') as file:
+                content = file.read()
+                key = os.path.splitext(filename)[0]
+                content_data[key] = content
+
+    return render(request, 'index.html', content_data)
